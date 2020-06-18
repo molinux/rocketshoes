12dadde90bd63be9167e01fa00391593d3420ba6
Molinux <cybercrowbr@gmail.com>
2020-06-18 14:36:03 -0300

    Refactoring using React Hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
@@ -9,73 +8,62 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-class Home extends Component {
-  state = {
-    products: [],
-  };
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const amount = useSelector((state) =>
+    state.cart.reduce((sumAmount, product) => {
+      // If amount doens't exists set to 0
+      sumAmount[product.id] = product.amount;
 
-  async componentDidMount() {
-    const response = await api.get('products');
+      return sumAmount;
+    }, {})
+  );
 
-    // Se colocar direto no <span>{product.price}<span> toda vez que recarregar o render
-    // vai executar a função. Dessa forma fica mais otimizado
-    const data = response.data.map((product) => ({
-      ...product,
-      priceFormated: formatPrice(product.price),
-    }));
+  const dispatch = useDispatch();
 
-    this.setState({ products: data });
-  }
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get('products');
 
-  handleAddProduct = (id) => {
-    const { addToCartRequest } = this.props;
+      // Se colocar direto no <span>{product.price}<span> toda vez que recarregar o render
+      // vai executar a função. Dessa forma fica mais otimizado
+      const data = response.data.map((product) => ({
+        ...product,
+        priceFormated: formatPrice(product.price),
+      }));
 
+      setProducts(data);
+    }
+
+    loadProducts();
+    // [] - array vazio para executar somente uma vez, quando o componente for montado
+  }, []);
+
+  // Como não precisa utilizar o useCallback pois não depende de nenhuma outra info
+  // somente o id que ela recebe
+  function handleAddProduct(id) {
     // Triggers Redux actions
-    addToCartRequest(id);
-  };
-
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
-
-    return (
-      <ProductList>
-        {products.map((product) => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormated}</span>
-
-            <button
-              type="button"
-              onClick={() => this.handleAddProduct(product.id)}
-            >
-              <div>
-                <MdAddShoppingCart size={16} color="#fff" />{' '}
-                {amount[product.id] || 0}
-              </div>
-
-              <span>ADICIONAR AO CARRINHO</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    );
+    dispatch(CartActions.addToCartRequest(id));
   }
+
+  return (
+    <ProductList>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormated}</span>
+
+          <button type="button" onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />{' '}
+              {amount[product.id] || 0}
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
-
-const mapStateToProps = (state) => ({
-  // ..., {}), -> Initialize the object empty
-  amount: state.cart.reduce((amount, product) => {
-    // If amount doens't exists set to 0
-    amount[product.id] = product.amount;
-
-    return amount;
-  }, {}),
-});
-
-const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(CartActions, dispatch);
-
-// Redux Connection
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
